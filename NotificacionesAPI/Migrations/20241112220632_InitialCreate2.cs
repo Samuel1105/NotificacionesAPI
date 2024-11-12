2ac81c8427aafc73d8a7834f39d2a672bfe925a4
Samuel <ame0030143@est.univalle.edu>
2024-11-12 18:10:55 -0400

    TIpo

---
+++ NotificacionesAPI/Migrations/20241112220632_InitialCreate2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotificacionesAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Tipo",
+                table: "Notifications",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tipo",
+                table: "Notifications");
+        }
+    }
+}
--- NotificacionesAPI/Models/NotificationModel.cs
+++ NotificacionesAPI/Models/NotificationModel.cs
@@ -28,6 +28,7 @@
         public string UserId { get; set; }
         public string Message { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public int Tipo { get; set; }
         public bool IsRead { get; set; } = false;
     }
     public class BroadcastNotificationModel
